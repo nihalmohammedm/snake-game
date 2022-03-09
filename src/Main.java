3fc6ebe960f2ab399365e482f1fe861328232f5c
nihalmohammedm <mnihu007@gmail.com>
2022-03-09 12:48:52 +0530

    Snake Game

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//Create Window
+		Window f1= new Window();
+		
+		// window settings
+		f1.setTitle("EatAsnack");
+		f1.setSize(300,400);
+		f1.setVisible(true);
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+
+	}
+}
